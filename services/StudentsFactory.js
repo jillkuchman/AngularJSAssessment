0f0893df865d5d4f95a5759c94c15371a213093a
Jill Kuchman <jillkuchman@gmail.com>
2015-04-24 09:36:14 -0700

    Add array of student objects to studentsFactory.js

---
+++ services/StudentsFactory.js
@@ -0,0 +1,17 @@
+signIn.factory('StudentsFactory', function StudentsFactory() {
+    var factory = {};
+    //hard-coded list of students
+    factory.students =  [
+                            {name: 'John', signedin: false},
+                            {name: 'Jane', signedin: false},
+                            {name: 'David', signedin: false},
+                            {name: 'Lucy', signedin: false},
+                            {name: 'Alex', signedin: false},
+                            {name: 'Greta', signedin: false},
+                            {name: 'Samson', signedin: false},
+                            {name: 'Ashley', signedin: false},
+                            {name: 'Tim', signedin: false},
+                            {name: 'Francine', signedin: false},
+                        ];
+    return factory;
+});
