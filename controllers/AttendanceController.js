da1f27af6e3b59dd47ea4694e67d46b1c532bd67
Jill Kuchman <jillkuchman@gmail.com>
2015-04-24 11:21:49 -0700

    Add navigation buttons to page footer. Add styles.css to apply css to navigation bar.

--- controllers/AttendanceController.js
+++ controllers/AttendanceController.js
@@ -1,10 +1,12 @@
 signIn.controller('AttendanceCtrl', function AttendanceCtrl($scope, StudentsFactory) {
     $scope.students = StudentsFactory.students;
 
+    //this method changes a student's signedin property to true
     $scope.signIn = function(student){
         student.signedin = true;
     };
 
+    //this method changes a student's signedin property to false
     $scope.signOut = function(student){
         student.signedin = false;
     };
