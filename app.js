e3c59f3493a8ea7493575e768e5ad0fa536f4abe
Jill Kuchman <jillkuchman@gmail.com>
2015-04-24 09:28:32 -0700

    Add routes and basic pages for sign in and attendance pages.

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 var signIn = angular.module('signIn', ['ui.router']);
 
 signIn.config(function($stateProvider) {
+
+    //route to home page
     $stateProvider.state('home', {
         url: '',
         templateUrl: 'partials/home.html'
+    });
+
+    //route to sign in page
+    $stateProvider.state('sign-in', {
+        url: 'sign-in',
+        templateUrl: 'partials/sign-in.html'
+    });
+
+    //route to attendance page
+    $stateProvider.state('attendance', {
+        url: 'attendance',
+        templateUrl: 'partials/attendance.html'
     });
 });
